f00db42d5ae5baa189845425597ec21d1c21aa76
GitHub Action <action@github.com>
2025-12-22 10:18:47 +0000

    Auto-format code with clang-format

--- ggml/src/ggml-cann/acl_tensor.h
+++ ggml/src/ggml-cann/acl_tensor.h
@@ -85,12 +85,12 @@ aclTensor* ggml_cann_create_tensor_transpose(void* data_ptr, aclDataType dtype,
 
 template <typename TYPE>
 aclTensor* ggml_cann_create_tensor(void* data_ptr, aclDataType dtype,
-                                   TYPE type_size, std::vector<int64_t>&& ne, std::vector<TYPE>&& nb,
-                                   int64_t dims,
+                                   TYPE type_size, std::vector<int64_t>&& ne,
+                                   std::vector<TYPE>&& nb, int64_t dims,
                                    aclFormat format = ACL_FORMAT_ND,
-                                   size_t offset = 0)
-{
-    ggml_cann_create_tensor(data_ptr, dtype, type_size, ne.data(), nb.data(), dims, format, offset);
+                                   size_t offset = 0) {
+    ggml_cann_create_tensor(data_ptr, dtype, type_size, ne.data(), nb.data(),
+                            dims, format, offset);
 }
 /**
  * @brief   Template for creating an ACL tensor from provided parameters.
