ecc12ead8336f5d7d4a21f8e4f1db99507038157
GitHub Action <action@github.com>
2026-01-19 21:09:18 +0000

    Auto-format code with clang-format

--- src/llama-model-loader.cpp
+++ src/llama-model-loader.cpp
@@ -842,7 +842,7 @@ struct ggml_tensor * llama_model_loader::create_tensor(struct ggml_context * ctx
         tensor = ggml_dup_tensor(ctx, cur);
     } else if (spliter.mode == LLAMA_QKV_SPLIT) {
         int64_t tar_ne[4] = { ne[0], ne[1], 1, 1 };
-        tensor = ggml_new_tensor(ctx, cur->type, GGML_MAX_DIMS, tar_ne);
+        tensor            = ggml_new_tensor(ctx, cur->type, GGML_MAX_DIMS, tar_ne);
     } else if (spliter.mode == LLAMA_AVG_SPLIT) {
         int     cur_split_dim = 0;
         int64_t cur_dim_size  = 1;
@@ -1340,20 +1340,19 @@ bool llama_model_loader::load_all_data_mpi(struct ggml_context * ctx, const stru
             }
             size_done += n_size;
         } else if (spliter->mode == LLAMA_QKV_SPLIT) {
-            
             GGML_ASSERT(spliter->post_process == NULL);
-            const int64_t element_size     = ggml_element_size(cur);
-            int64_t q_size = spliter->qkv_param.q_size * element_size;
-            int64_t k_size = spliter->qkv_param.k_size * element_size;
-            int64_t v_size = spliter->qkv_param.v_size * element_size;
-            int64_t q_offset_dst = spliter->qkv_param.q_offset_dst * element_size;
-            int64_t k_offset_dst = spliter->qkv_param.k_offset_dst * element_size;
-            int64_t v_offset_dst = spliter->qkv_param.v_offset_dst * element_size;
-            int64_t q_offset_src = spliter->qkv_param.q_offset_src * element_size;
-            int64_t k_offset_src = spliter->qkv_param.k_offset_src * element_size;
-            int64_t v_offset_src = spliter->qkv_param.v_offset_src * element_size;
-            int64_t src_size = spliter->qkv_param.tot_size_src * element_size;
-            
+            const int64_t element_size = ggml_element_size(cur);
+            int64_t       q_size       = spliter->qkv_param.q_size * element_size;
+            int64_t       k_size       = spliter->qkv_param.k_size * element_size;
+            int64_t       v_size       = spliter->qkv_param.v_size * element_size;
+            int64_t       q_offset_dst = spliter->qkv_param.q_offset_dst * element_size;
+            int64_t       k_offset_dst = spliter->qkv_param.k_offset_dst * element_size;
+            int64_t       v_offset_dst = spliter->qkv_param.v_offset_dst * element_size;
+            int64_t       q_offset_src = spliter->qkv_param.q_offset_src * element_size;
+            int64_t       k_offset_src = spliter->qkv_param.k_offset_src * element_size;
+            int64_t       v_offset_src = spliter->qkv_param.v_offset_src * element_size;
+            int64_t       src_size     = spliter->qkv_param.tot_size_src * element_size;
+
             // printf("mlj: reach load qkv!");
             // printf("mlj: tp_id = %d, q_offset = %lld, k_offset = %lld, v_offset = %lld\n",spliter->tp_id, q_offset_src, k_offset_src, v_offset_src);
 
